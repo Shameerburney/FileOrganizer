d8fd30a6728af3d092ebd694e77bd59bca08b872
SB <136708729+Shameerburney@users.noreply.github.com>
2025-10-23 14:30:45 +0500

    download.py

--- download.py
+++ download.py
@@ -4,6 +4,7 @@ st.title("📂 Download File Organizer Desktop App")
 st.write("Click the button below to download the desktop app and organize your folders locally.")
 
 # URL to your .exe on GitHub Releases
-exe_url = "https://github.com/shameerburney/file-organizer/releases/download/v1.0/file_organizer.exe"
+exe_url = "https://github.com/Shameerburney/FileOrganizer/releases/download/v1/file_organizer.exe"
 
 st.markdown(f"[⬇️ Download File Organizer]({exe_url})")
+
